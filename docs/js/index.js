26f5c7189a9cd4150b8c866a798d862d4895b86e
kobataku <tk_musik@yahoo.co.jp>
2020-09-23 00:31:29 +0900

    #4 背景色、帯の幅、帯の色を時刻ごとに

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,7 +1,12 @@
+const MORNING_TIME = [4, 10]
+const AROUNDNOON_TIME = [10, 16]
+const EVENING_TIME = [16, 19]
+// NIGHT_TIMEはそのほかとする。（24時をはさむのでめんどい）
 const vm = new Vue({
   el: "#clock",
   data: {
     date: Date.now(),
+    // hue: 0,
   },
   computed: {
     hour: (vm) => new Date(vm.date).getHours(),
@@ -13,16 +18,32 @@ const vm = new Vue({
     hDashArray: (vm) => {
       const h = vm.ampm === "am" ? vm.hour : vm.hour - 12;
       const p = ((h + vm.min / 60) / 12) * 100;
-      return createDashArray(p, 1 / 3);
+      return createDashArray(p, 1 / 2);
     },
     mDashArray: (vm) => {
       const p = ((vm.min + vm.sec / 60) / 60) * 100;
-      return createDashArray(p, 2 / 3);
+      return createDashArray(p, 5 / 6);
     },
     sDashArray: (vm) => {
       const p = ((vm.sec + vm.ms / 1000) / 60) * 100;
-      return createDashArray(p, 1);
+      return createDashArray(p, 7 / 6);
     },
+    hueOfTime: (vm) => {
+      let hue = 0;
+      if (isInRange(vm.hour, ...MORNING_TIME)) {
+        hue = 188;
+      } else if (isInRange(vm.hour, ...AROUNDNOON_TIME)) {
+        hue = 47;
+      } else if (isInRange(vm.hour, ...EVENING_TIME)) {
+        hue = 1;
+      } else {
+        hue = 266;
+      }
+      return hue;
+    },
+    hHSL: (vm) => `hsl(${vm.hueOfTime}, 100%, 70%)`,
+    mHSL: (vm) => `hsl(${vm.hueOfTime}, 100%, 50%)`,
+    sHSL: (vm) => `hsl(${vm.hueOfTime}, 100%, 40%)`,
   },
 });
 
@@ -37,6 +58,11 @@ function createDashArray(p, correction) {
 function tick() {
   vm.date = Date.now();
   requestAnimationFrame(tick);
+}
+
+/** vがmin以上max未満ならtrue */
+function isInRange(v, min, max) {
+  return v >= min && v < max
 }
 
 tick();
