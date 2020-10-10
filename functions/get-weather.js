69630ceeb21f77b720fbf50db2245efe9cb5a246
kobataku <tk_musik@yahoo.co.jp>
2020-10-10 15:32:14 +0900

    endpoint typo

--- functions/get-weather.js
+++ functions/get-weather.js
@@ -1,7 +1,6 @@
 import fetch from "node-fetch";
 
-const API_ENDPOINT =
-  "https://openweathermap.org/api.openweathermap.org/data/2.5/weather";
+const API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather";
 
 exports.handler = async (event, context) => {
   const lat = event.queryStringParameters.lat;
@@ -9,7 +8,7 @@ exports.handler = async (event, context) => {
   const { OSM_API_KEY } = process.env;
   console.log(lat, lon, OSM_API_KEY);
   if (lat && lon && OSM_API_KEY) {
-    const url = `${API_ENDPOINT}?lat=${lat}&lon=${lon}&appid=${OSM_API_KEY}`
+    const url = `${API_ENDPOINT}?lat=${lat}&lon=${lon}&appid=${OSM_API_KEY}`;
     return fetch(url, { headers: { Accept: "application/json" } })
       .then((response) => response.json())
       .then((data) => ({
