a743d821e5d8de2254cbccd375ad47bd046510fc
kobataku <tk_musik@yahoo.co.jp>
2020-10-10 16:26:13 +0900

    hide error

--- functions/get-weather.js
+++ functions/get-weather.js
@@ -6,7 +6,6 @@ exports.handler = async (event, context) => {
   const lat = event.queryStringParameters.lat;
   const lon = event.queryStringParameters.lon;
   const { OSM_API_KEY } = process.env;
-  console.log(lat, lon, OSM_API_KEY);
   if (lat && lon && OSM_API_KEY) {
     const url = `${API_ENDPOINT}?lat=${lat}&lon=${lon}&appid=${OSM_API_KEY}`;
     return fetch(url, { headers: { Accept: "application/json" } })
@@ -21,6 +20,9 @@ exports.handler = async (event, context) => {
           body: JSON.stringify(data),
         };
       })
-      .catch((error) => ({ statusCode: 422, body: String(error) }));
+      .catch((error) => {
+        console.log(String(error));
+        return { statusCode: 422, body: "error occur. see server logs." };
+      });
   }
 };
